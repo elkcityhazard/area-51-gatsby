3192a2a58887f8d06bb9723638d79c3fe8480de3
Andrew McCall <andrew.m.mccall@gmail.com>
2021-07-29 15:32:44 -0400

    removed strapi

--- area-51-strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '548c67ed83e774c55d3fe793939ae7a7'),
-    },
-  },
-});
--- area-51-strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '1ff30024-0c70-46d0-8e3c-6770ef81db38'
-};
